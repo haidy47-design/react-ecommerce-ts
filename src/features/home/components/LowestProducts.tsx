df7181e2b603a7e0975f806ea43ab2305a10a0e7
haidy47-design <haidy77749@gmail.com>
2025-10-06 14:43:44 +0200

    Initial commit

---
+++ src/features/home/components/LowestProducts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LowestProducts = () => {
+  return (
+    <div>LowestProducts</div>
+  )
+}
+
+export default LowestProducts
---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
